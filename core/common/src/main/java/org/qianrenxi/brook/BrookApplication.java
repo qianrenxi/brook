14a2dcc5bdcb5b175f562cddcff489418d5e617f
Tony <tonyeen@foxmail.com>
2017-02-14 02:42:53 +0800

    add sub modeule

---
+++ core/common/src/main/java/org/qianrenxi/brook/BrookApplication.java
@@ -0,0 +1,13 @@
+package org.qianrenxi.brook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrookApplication {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(BrookApplication.class, args);
+	}
+
+}
